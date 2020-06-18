0a67edb914b729400d0a0527ee728bf39d31090f
JM <manzur-1990@hotmail.com>
2020-06-18 12:01:30 -0500

    Change Title, removed serverless function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+  
+  
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Jorge ManzurI</h1>
       <h2>
         Deployed with{' '}
         <a
@@ -49,7 +41,7 @@ function App() {
       </p>
       <br />
       <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+     
     </main>
   );
 }
